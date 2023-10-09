f27546b58d29fbc36f27299c3802697e051f0049
Simon <simon@7lieues.io>
2023-10-09 16:16:44 +0200

    build: init app

---
+++ lib/scrapex.ex
@@ -0,0 +1,18 @@
+defmodule Scrapex do
+  @moduledoc """
+  Documentation for `Scrapex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scrapex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/scrapex_test.exs
@@ -0,0 +1,8 @@
+defmodule ScrapexTest do
+  use ExUnit.Case
+  doctest Scrapex
+
+  test "greets the world" do
+    assert Scrapex.hello() == :world
+  end
+end
