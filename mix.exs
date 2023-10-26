69e982de8479182a18b0c5c6ff13b5682284c828
Simon <simon@7lieues.io>
2023-10-26 13:03:39 +0200

    build: adds Swoosh and Finch to send job reports by mail

--- mix.exs
+++ mix.exs
@@ -23,7 +23,9 @@ defmodule Scrapex.MixProject do
   defp deps do
     [
       {:crawly, "~> 0.16.0"},
-      {:floki, "~> 0.34.3"}
+      {:finch, "~> 0.16.0"},
+      {:floki, "~> 0.34.3"},
+      {:swoosh, "~> 1.12"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
